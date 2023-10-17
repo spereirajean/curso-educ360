d2db5bcb513085e37ea254242354f4f812607660
Jean silva <spereira.jean@outlook.com>
2023-10-16 22:43:47 -0300

    Initial commit

---
+++ src/br/com/cdb/estruturadedados/EstruturaDeDados01.java
@@ -0,0 +1,21 @@
+package br.com.cdb.estruturadedados;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class EstruturaDeDados01 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<Integer> notaAlunos = new ArrayList<>();
+
+        notaAlunos.add(78);
+        notaAlunos.add(75);
+        notaAlunos.add(80);
+        notaAlunos.add(97);
+        notaAlunos.add(64);
+        notaAlunos.add(88);
+
+    }
+}
